7b3942b4fca87007f078ec0ec98266e9f9ee4259
Thunwa007 <vathunwa@gmail.com>
2021-02-26 01:10:18 +0700

    update last

--- util/asyncComponent.js
+++ util/asyncComponent.js
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic';
-import Loading from "../components/Loading";
+import Loading from "../Components/Loading";
 import React from "react";
 
 export default function asyncComponent(importComponent) {
